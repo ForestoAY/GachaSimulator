0bbdee5eb1d964686a22a758036051512df738f0
ForestoAY <forestoay@gmail.com>
2024-07-17 16:11:18 +0700

    first commit

---
+++ gachaItem.js
@@ -0,0 +1,28 @@
+let gachaItem = [
+  {
+    nama: "R",
+    rarity: "60%",
+    linkGambar: "https://cdn-icons-png.flaticon.com/512/4553/4553016.png",
+    id: 1,
+  },
+  {
+    nama: "SR",
+    rarity: "30%",
+    linkGambar: "https://img.lovepik.com/element/45016/3146.png_860.png",
+    id: 2,
+  },
+  {
+    nama: "SSR",
+    rarity: "8%",
+    linkGambar:
+      "https://pbs.twimg.com/profile_images/1667097609624866816/GQmBXyII_400x400.jpg",
+    id: 3,
+  },
+  {
+    nama: "EX",
+    rarity: "2%",
+    linkGambar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIVZKlV_rZdJ4bvLs1FyrmPL5YpV3A9pihrA&s",
+    id: 4,
+  },
+];
---
+++ script.js
@@ -0,0 +1,50 @@
+let gachaItem = [
+  {
+    nama: "R",
+    rarity: "60%",
+    linkGambar: "https://cdn-icons-png.flaticon.com/512/4553/4553016.png",
+    id: 1,
+  },
+  {
+    nama: "SR",
+    rarity: "30%",
+    linkGambar: "https://img.lovepik.com/element/45016/3146.png_860.png",
+    id: 2,
+  },
+  {
+    nama: "SSR",
+    rarity: "8%",
+    linkGambar:
+      "https://pbs.twimg.com/profile_images/1667097609624866816/GQmBXyII_400x400.jpg",
+    id: 3,
+  },
+  {
+    nama: "EX",
+    rarity: "2%",
+    linkGambar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIVZKlV_rZdJ4bvLs1FyrmPL5YpV3A9pihrA&s",
+    id: 4,
+  },
+];
+
+function render(array) {
+  let itemList = document.getElementById("itemList");
+  itemList.innerHTML = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let perCharacter = array[i];
+
+    let { nama, rarity, linkGambar, id } = perCharacter;
+
+    itemList.innerHTML += `
+    <div class="card rare" style="width: 10rem; background-color: rgb(54, 54, 201);">
+      <img src="${linkGambar}" class="card-img-top" alt="${nama}">
+      <div class="card-body">
+        <p>${nama}</p>
+        <p>${rarity}</p>
+      </div>
+    </div>`;
+  }
+}
+
+render(gachaItem);
